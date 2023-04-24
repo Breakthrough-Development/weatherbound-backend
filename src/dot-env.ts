4f015ab03a108ad84fcd0a82ed1b8b64dcdb9fbf
Royer Adames <royeraadames@gmail.com>
2023-04-24 17:03:02 -0400

    check that all the .env are there and give an example input based on the .env-template

---
+++ src/dot-env.ts
@@ -0,0 +1,22 @@
+import * as dotenv from "dotenv";
+
+export function dotEnv() {
+  const envFileLoadResult = dotenv.config({ path: '.env' });
+  if (envFileLoadResult.error) {
+    console.error(`Error: Could not load environment variables from .env file. ${envFileLoadResult.error}`);
+    process.exit(1);
+  }
+
+  // check that all the .env are there and give an example input based on the .env-template
+  const envTemplate = dotenv.config({ path: '.env-template' });
+  let requiredEnvVars: {key: string, example: string}[] = []
+  for (const key in envTemplate.parsed){
+    requiredEnvVars.push({key, example: envTemplate.parsed[key]})
+  }
+  for (const envVar of requiredEnvVars) {
+    if (!process.env[envVar.key]) {
+      console.error(`Error: ${envVar.key} environment variable is not defined. Please set the ${envVar.key} environment variable to a value. For example: ${envVar.example}`);
+      process.exit(1);
+    }
+  }
+}
