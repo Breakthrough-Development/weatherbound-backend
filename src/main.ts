2b023b43c16be23e9d6603b61a8690dc36eaf090
Royer Adames <royeraadames@gmail.com>
2023-04-24 11:07:03 -0400

    require the environment variables

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,23 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
 
+dotenv.config({ path: '.env' }); // Load environment variables from .env file
+
+const requiredEnvVars = [
+  { key: 'GOOGLE_CLIENT_ID', example: 'abc123xyz' },
+  { key: 'GOOGLE_CLIENT_SECRET', example: 'secret123' },
+  { key: 'PORT', example: '3000' },
+];
+
+for (const envVar of requiredEnvVars) {
+  if (!process.env[envVar.key]) {
+    console.error(`Error: ${envVar.key} environment variable is not defined.`);
+    console.error(`Please set the ${envVar.key} environment variable to a value. For example: ${envVar.example}`);
+    process.exit(1);
+  }
+}
+
 async function bootstrap() {
-  dotenv.config();
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT);
 }
