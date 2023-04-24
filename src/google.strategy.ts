671614ee010ad3f3605fd26036a841d381afec2e
Royer Adames <royeraadames@gmail.com>
2023-04-24 15:45:48 -0400

    find the user in the db or add them to it

--- src/google.strategy.ts
+++ src/google.strategy.ts
@@ -1,24 +1,33 @@
-import { Injectable } from '@nestjs/common';
-import { PassportStrategy } from '@nestjs/passport';
-import { Strategy } from 'passport-google-oauth20';
+import { Injectable } from "@nestjs/common";
+import { PassportStrategy } from "@nestjs/passport";
+import { Strategy } from "passport-google-oauth20";
 import { ProfileInterface } from "./model/profile.interface";
+import { UsersService } from "./services/users.service";
+import { User } from "./user.entity";
 
 @Injectable()
-export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
-  constructor() {
+export class GoogleStrategy extends PassportStrategy(Strategy, "google") {
+  constructor(private readonly usersService: UsersService) {
     super({
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
       callbackURL: `${process.env.DOMAIN}:${process.env.PORT}/auth/google/callback`,
-      scope: ['email', 'profile'],
+      scope: ["email", "profile"]
     });
   }
 
   async validate(accessToken: string, refreshToken: string, profile: ProfileInterface) {
-    // Implement your validation logic here
-    return {
-      email: profile.emails[0].value,
-      name: profile.displayName,
-    };
+    const {
+      emails: [{ value: email }],
+      displayName: name,
+      photos: [{ value: photo }] = [{}]
+    } = profile;
+
+    // Check if the user already exists in the database
+    return await this.usersService.findUserByEmail(email) || await this.usersService.createUser({
+      email,
+      name,
+      photo
+    } as User);
   }
 }
