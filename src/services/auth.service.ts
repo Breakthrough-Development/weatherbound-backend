f66a5327faa245af98a6c6f7f659669369daf00c
Royer Adames <royeraadames@gmail.com>
2023-04-24 19:18:09 -0400

    signJwt and verifyJwt

---
+++ src/services/auth.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+
+@Injectable()
+export class AuthService {
+  constructor(private jwtService: JwtService) {}
+
+  signJwt(userId: number): string {
+    const payload = { userId };
+    return this.jwtService.sign(payload);
+  }
+
+  verifyJwt(token: string): { userId: number } {
+    return this.jwtService.verify<{ userId: number }>(token);
+  }
+}
