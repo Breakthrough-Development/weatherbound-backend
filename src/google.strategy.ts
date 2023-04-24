af5d5502e4ed4ff5199fb71aad2703b7e5670409
Royer Adames <royeraadames@gmail.com>
2023-04-24 10:06:45 -0400

    add secret and id by .env

--- src/google.strategy.ts
+++ src/google.strategy.ts
@@ -2,13 +2,12 @@ import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { Strategy } from 'passport-google-oauth20';
 
-// todo: Replace your-client-id and your-client-secret with your Google OAuth client ID and secret. This sets up the Google authentication strategy using the passport-google-oauth20 module. It also defines the validate method, which is called after the user is authenticated.
 @Injectable()
 export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
   constructor() {
     super({
-      clientID: 'your-client-id',
-      clientSecret: 'your-client-secret',
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
       callbackURL: 'http://localhost:3000/auth/google/callback',
       scope: ['email', 'profile'],
     });
