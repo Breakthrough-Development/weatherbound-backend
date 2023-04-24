5112f514f7fb4ab46df98b3013b2e31a13f08081
Royer Adames <royeraadames@gmail.com>
2023-04-24 17:04:35 -0400

    redirect to WEB_REDIRECT_URL when callback is used

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,23 +1,27 @@
-import { Controller, Get, UseGuards, Req, } from '@nestjs/common';
-import { AppService } from './app.service';
-import { AuthGuard } from '@nestjs/passport';
+import { Controller, Get, Req, Res, UseGuards } from "@nestjs/common";
+import { AppService } from "./app.service";
+import { AuthGuard } from "@nestjs/passport";
+import { Response } from "express";
+
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) {
+  }
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
   }
 
-  @Get('auth/google')
-  @UseGuards(AuthGuard('google'))
-  async googleAuth() {}
+  @Get("auth/google")
+  @UseGuards(AuthGuard("google"))
+  async googleAuth() {
+  }
 
-  @Get('auth/google/callback')
-  @UseGuards(AuthGuard('google'))
-  googleAuthRedirect(@Req() req) {
+  @Get("auth/google/callback")
+  @UseGuards(AuthGuard("google"))
+  googleAuthRedirect(@Req() req, @Res() res: Response): void {
     // Handle the Google authentication callback
-    return { message: 'Google authentication successful!' };
+    res.redirect(process.env.WEB_REDIRECT_URL);
   }
 }
