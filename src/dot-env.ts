6dc6558465ebc374f9e22273f8f662dbf12cddb7
Royer Adames <royeraadames@gmail.com>
2023-04-24 20:17:04 -0400

    make it loop through all the missing keys

--- src/dot-env.ts
+++ src/dot-env.ts
@@ -1,4 +1,5 @@
-import * as dotenv from "dotenv";
+import * as dotenv from 'dotenv';
+import * as fs from 'fs';
 
 export function dotEnv() {
   const envFileLoadResult = dotenv.config({ path: '.env' });
@@ -7,17 +8,34 @@ export function dotEnv() {
     process.exit(1);
   }
 
-  //todo: fix bug: const envTemplate = dotenv.config({ path: '.env-template' }); is loading in the template variables so there is nocase when the error will load
-  // check that all the .env are there and give an example input based on the .env-template
-  const envTemplate = dotenv.config({ path: '.env-template' });
-  let requiredEnvVars: {key: string, example: string}[] = []
-  for (const key in envTemplate.parsed){
-    requiredEnvVars.push({key, example: envTemplate.parsed[key]})
-  }
-  for (const envVar of requiredEnvVars) {
-    if (!process.env[envVar.key]) {
-      console.error(`Error: ${envVar.key} environment variable is not defined. Please set the ${envVar.key} environment variable to a value. For example: ${envVar.example}`);
-      process.exit(1);
+  // Read the contents of the .env-template file
+  const envTemplateContent = fs.readFileSync('.env-template', { encoding: 'utf-8' });
+
+  // Split the content by lines
+  const envTemplateLines = envTemplateContent.split('\n');
+
+  // Array to store the missing keys
+  const missingKeys: string[] = [];
+
+  // Iterate through the lines
+  for (const line of envTemplateLines) {
+    // Skip empty lines and comments
+    if (line.trim() === '' || line.startsWith('#')) {
+      continue;
     }
+
+    // Extract the key and example value
+    const [key, example] = line.split('=');
+
+    // Check if the key is present in the environment variables
+    if (!process.env[key]) {
+      missingKeys.push(`${key} (example: ${example})`);
+    }
+  }
+
+  // If there are missing keys, display an error message and exit
+  if (missingKeys.length > 0) {
+    console.error(`Error: The following environment variables are not defined:\n${missingKeys.join('\n')}\nPlease set the missing environment variables.`);
+    process.exit(1);
   }
 }
