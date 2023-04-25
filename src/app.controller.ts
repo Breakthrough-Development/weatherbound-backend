701e938eee0fd8e679935f96b78d730cee614172
Royer Adames <royeraadames@gmail.com>
2023-04-24 20:10:13 -0400

    store the JWT token into the user browser

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -21,6 +21,14 @@ export class AppController {
   @Get("auth/google/callback")
   @UseGuards(AuthGuard("google"))
   googleAuthRedirect(@Req() req, @Res() res: Response): void {
+    // Set the JWT token as a cookie in the response
+    res.cookie('token', req['token'], {
+      maxAge: 86400000, // 24 hours
+      httpOnly: true,
+      secure: true, // Set this to true in production
+      sameSite: 'strict',
+    });
+
     // Handle the Google authentication callback
     res.redirect(process.env.WEB_REDIRECT_URL);
   }
--- src/google.strategy.ts
+++ src/google.strategy.ts
@@ -1,46 +1,56 @@
-import { Injectable, Res } from "@nestjs/common";
-import { PassportStrategy } from "@nestjs/passport";
-import { Strategy } from "passport-google-oauth20";
-import { ProfileInterface } from "./model/profile.interface";
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { Strategy } from 'passport-google-oauth20';
+import { Profile as ProfileInterface } from 'passport';
 import { UsersService } from "./services/users.service";
-import { User } from "./entities/user.entity";
 import { AuthService } from "./services/auth.service";
-import { Response } from 'express';
+import { User } from "./entities/user.entity";
 
 @Injectable()
-export class GoogleStrategy extends PassportStrategy(Strategy, "google") {
-  constructor(private readonly usersService: UsersService, private readonly authService: AuthService,) {
+export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
+  constructor(
+    private readonly usersService: UsersService,
+    private readonly authService: AuthService,
+  ) {
     super({
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
       callbackURL: `${process.env.DOMAIN}:${process.env.PORT}/auth/google/callback`,
-      scope: ["email", "profile"]
+      scope: ['email', 'profile'],
+      passReqToCallback: true, // Add this line to pass the request object to the callback
     });
   }
 
-  async validate(accessToken: string, refreshToken: string, profile: ProfileInterface, @Res() res: Response) {
+  async validate(
+    req: Request,
+    accessToken: string,
+    refreshToken: string,
+    profile: ProfileInterface,
+    done: (error: any, user?: any, info?: any) => void, // Add the "done" callback parameter
+  ) {
     const {
       emails: [{ value: email }],
       displayName: name,
-      photos: [{ value: photo }] = [{}]
+      photos: [{ value: photo } = {} as { value?: string }], // Update the default value here
     } = profile;
 
+
     // Check if the user already exists in the database
-    const user =  await this.usersService.findUserByEmail(email) || await this.usersService.createUser({
-      email,
-      name,
-      photo
-    } as User);
+    const user =
+      (await this.usersService.findUserByEmail(email)) ||
+      (await this.usersService.createUser({
+        email,
+        name,
+        photo,
+      } as User));
 
     // Create a JWT token with user ID
     const token = this.authService.signJwt(user.id);
 
-    // Set the JWT token as a cookie in the response
-    res.cookie('token', token, {
-      maxAge: 86400000, // 24 hours
-      httpOnly: true,
-      secure: true, // Set this to true in production
-      sameSite: 'strict',
-    });
+    // Attach the token to the request object
+    req['token'] = token;
+
+    // Call the "done" callback with the user object
+    done(null, user);
   }
 }
