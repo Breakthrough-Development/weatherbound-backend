5038e3af182a7247001b977517fd043c8b5c3603
Royer Adames <royeraadames@gmail.com>
2023-04-26 13:51:15 -0400

    move the settings service to the controller, and have a endpoint that gets all the data

--- src/modules/weather/weather.controller.ts
+++ src/modules/weather/weather.controller.ts
@@ -3,18 +3,27 @@ import { WeatherService } from './weather.service';
 import { JwtAuthGuard } from '../../auth/jwt-auth.guard';
 import { UserEntity } from '../user/user.entity';
 import { GetUser } from '../../auth/get-user.decorator';
+import { SettingsService } from '../settings/settings.service';
 
 @Controller('weather')
 @UseGuards(JwtAuthGuard)
 export class WeatherController {
-  constructor(private readonly weatherService: WeatherService) {}
+  constructor(
+    private readonly weatherService: WeatherService,
+    private readonly settingsService: SettingsService,
+  ) {}
 
   @Get('autocomplete')
   async getLocationAutocomplete(
     @GetUser() user: UserEntity,
     @Query('query') query: string,
   ) {
-    return await this.weatherService.getLocationAutocomplete(user, query);
+    const settings = await this.settingsService.findByUser(user);
+    return await this.weatherService.getLocationAutocomplete(
+      user,
+      query,
+      settings,
+    );
   }
 
   @Get('daily')
@@ -22,7 +31,8 @@ export class WeatherController {
     @GetUser() user: UserEntity,
     @Query('query') query: string,
   ) {
-    return await this.weatherService.getDailyForecast(user, query);
+    const settings = await this.settingsService.findByUser(user);
+    return await this.weatherService.getDailyForecast(user, query, settings);
   }
 
   @Get('weekly')
@@ -30,7 +40,8 @@ export class WeatherController {
     @GetUser() user: UserEntity,
     @Query('query') query: string,
   ) {
-    return await this.weatherService.getWeeklyForecast(user, query);
+    const settings = await this.settingsService.findByUser(user);
+    return await this.weatherService.getWeeklyForecast(user, query, settings);
   }
 
   @Get('current')
@@ -38,7 +49,27 @@ export class WeatherController {
     @GetUser() user: UserEntity,
     @Query('query') query: string,
   ) {
-    console.log('query:', query);
-    return await this.weatherService.getCurrentWeather(user, query);
+    const settings = await this.settingsService.findByUser(user);
+    return await this.weatherService.getCurrentWeather(user, query, settings);
+  }
+
+  @Get('forecast')
+  async getForeCast(
+    @GetUser() user: UserEntity,
+    @Query('query') query: string,
+  ) {
+    const settings = await this.settingsService.findByUser(user);
+
+    const [current, weekly, daily] = await Promise.all([
+      this.weatherService.getCurrentWeather(user, query, settings),
+      this.weatherService.getWeeklyForecast(user, query, settings),
+      this.weatherService.getDailyForecast(user, query, settings),
+    ]);
+
+    return {
+      current,
+      daily,
+      weekly,
+    };
   }
 }
--- src/modules/weather/weather.service.ts
+++ src/modules/weather/weather.service.ts
@@ -3,35 +3,47 @@ import { SettingsService } from '../settings/settings.service';
 import axios from 'axios';
 import * as CryptoJS from 'crypto-js';
 import { UserEntity } from '../user/user.entity';
+import { SettingsEntity } from '../settings/settings.entity';
 
 @Injectable()
 export class WeatherService {
   constructor(private readonly settingsService: SettingsService) {}
 
-  async getLocationAutocomplete(user: UserEntity, query: string) {
-    const settings = await this.settingsService.findByUser(user);
+  async getLocationAutocomplete(
+    user: UserEntity,
+    query: string,
+    settings: SettingsEntity,
+  ) {
     const url = `${settings.weatherApiUrl}/autocomplete?access_key=${settings.apiKey}&query=${query}`;
     const response = await axios.get(url);
     return response.data;
   }
 
-  async getDailyForecast(user: UserEntity, query: string) {
-    const settings = await this.settingsService.findByUser(user);
+  async getDailyForecast(
+    user: UserEntity,
+    query: string,
+    settings: SettingsEntity,
+  ) {
     const url = `${settings.weatherApiUrl}/forecast?access_key=${settings.apiKey}&query=${query}&forecast_days=1&hourly=1`;
     const response = await axios.get(url);
     return response.data;
   }
 
-  async getWeeklyForecast(user: UserEntity, query: string) {
-    const settings = await this.settingsService.findByUser(user);
+  async getWeeklyForecast(
+    user: UserEntity,
+    query: string,
+    settings: SettingsEntity,
+  ) {
     const url = `${settings.weatherApiUrl}/forecast?access_key=${settings.apiKey}&query=${query}&forecast_days=7&hourly=24`;
     const response = await axios.get(url);
     return response.data;
   }
 
-  async getCurrentWeather(user: UserEntity, query: string) {
-    console.log(query);
-    const settings = await this.settingsService.findByUser(user);
+  async getCurrentWeather(
+    user: UserEntity,
+    query: string,
+    settings: SettingsEntity,
+  ) {
     const url = `${settings.weatherApiUrl}/current?access_key=${settings.apiKey}&query=${query}`;
     const response = await axios.get(url);
     return response.data;
