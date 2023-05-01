6a9028de81b8ddcf0635265e3b730599c8f2a0d5
Royer Adames <royeraadames@gmail.com>
2023-05-01 10:54:54 -0400

    require the length to be at least 5

--- src/modules/settings/dto/update-settings.dto.ts
+++ src/modules/settings/dto/update-settings.dto.ts
@@ -1,9 +1,11 @@
-import { IsString } from 'class-validator';
+import { IsString, MinLength } from 'class-validator';
 
 export class UpdateSettingsDto {
   @IsString()
+  @MinLength(5)
   readonly apiKey: string;
 
   @IsString()
+  @MinLength(5)
   readonly weatherApiUrl: string;
 }
