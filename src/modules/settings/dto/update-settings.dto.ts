f0d6815eeb7539b663cebada53e054671f3cf7da
Royer Adames <royeraadames@gmail.com>
2023-05-02 07:58:24 -0400

    allow any length size

--- src/modules/settings/dto/update-settings.dto.ts
+++ src/modules/settings/dto/update-settings.dto.ts
@@ -1,11 +1,9 @@
-import { IsString, MinLength } from 'class-validator';
+import { IsString } from 'class-validator';
 
 export class UpdateSettingsDto {
   @IsString()
-  @MinLength(5)
   readonly apiKey: string;
 
   @IsString()
-  @MinLength(5)
   readonly weatherApiUrl: string;
 }
